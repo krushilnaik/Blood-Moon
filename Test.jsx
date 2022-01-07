82f22c4e51bbb3bc6c53bca440796c2bdf9bf153
Krushil Naik <krushilnaik96@gmail.com>
2022-01-07 09:26:27 -0500

    Restructure theme, styling tweaks

---
+++ Test.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Test() {
+	return <div></div>;
+}
+
+export default Test;
+
+// test comment
+for (let index = 0; index < array.length; index++) {
+	const element = array[index];
+}
